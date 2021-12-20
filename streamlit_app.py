f3f176897ae74105566caf8bc873e34384f32730
Alex Sliver <alexsilversword@gmail.com>
2021-12-20 20:34:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import os
 
 """
 # Welcome to Streamlit!
@@ -17,22 +18,42 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    st.title('Columbus Systems')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    path2= "CWTP NF streamlit.xlsx"
+    @st.cache
+    def load_data(path2):
+        df=pd.read_excel(path2,index_col="Recd Date",parse_dates=True)
+        return df
 
-    points_per_turn = total_points / num_turns
+    df=load_data(path2)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    system = df['Systems'].sort_values(ascending=True).drop_duplicates()
+    system_choice = st.sidebar.multiselect('Select your system(s):', system)
+    # print(system_choice)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+    result1 = df[(df["Systems"].isin(system_choice))]
+
+    # result1 = result1.dropna(axis=1, thresh=int(result1.shape[1]*0.95))
+    result1 = result1.dropna(axis=1, how="all")
+    result1["Recd Date"] = result1.index
+
+    for i in result1.columns[2:]:
+        if i != "Recd Date" and i != "Temperature":
+            selection = alt.selection_single(on='mouseover', nearest=True)
+            # selection = alt.selection_multi(fields=['Origin'])
+            chart = alt.Chart(result1).mark_line(point=True).encode(
+            x=alt.X("Recd Date"),
+            y=alt.Y(i),
+            color=alt.Color("Systems:N"),
+            tooltip = ["Recd Date",i]
+            ).properties(title=i
+            ).add_selection(selection)
+            st.altair_chart(chart, use_container_width=True)
+
+
+
+        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+            .mark_circle(color='#0068c9', opacity=0.5)
+            .encode(x='x:Q', y='y:Q'))
